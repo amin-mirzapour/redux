14bd71f35f76524164fd453b8a235931fc552a42
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-21 16:22:08 +0330

    rReducers

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ const CAKE_ORDERED = 'CAKE_ORDERED';
 function orderCake() {
   return {
     type: CAKE_ORDERED,
+    quanity: 1,
   };
 }
+
+const initialState = {
+  numOfCakes: 10,
+  //   anotherProperty : 1 , only to show that you have to make a copy in reducer first then change the state
+};
+
+// (previousState , action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      state;
+  }
+};
