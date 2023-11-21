4846a71b4bb89f3e3cd00d4680c7d6dbb6fbd515
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-21 16:59:52 +0330

    Store

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import { legacy_createStore as createStore } from 'redux';
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
+// action &action creator
 function orderCake() {
   return {
     type: CAKE_ORDERED,
@@ -7,6 +10,7 @@ function orderCake() {
   };
 }
 
+// Reducer
 const initialState = {
   numOfCakes: 10,
   //   anotherProperty : 1 , only to show that you have to make a copy in reducer first then change the state
@@ -21,6 +25,20 @@ const reducer = (state = initialState, action) => {
         numOfCakes: state.numOfCakes - 1,
       };
     default:
-      state;
+      return state;
   }
 };
+
+// Store
+const store = createStore(reducer);
+console.log('initial State', store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log('update Store', store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
